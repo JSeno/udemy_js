6ad48bc9436284ed1166f8a038e08f60ea68d622
Jefferson Seno <jefferson.seno@datacake.com.br>
2022-06-05 10:06:46 -0300

    Iniciando aulas de Javascript iniciantes.

---
+++ aula03/index.js
@@ -0,0 +1,8 @@
+console.log('Jefferson'); // String = Texto
+console.log('Jefferson "Seno"')
+console.log(`'Jefferson' "Seno"`);
+
+console.log(35, 15.85, 'Jefferson');
+
+// Exercício  da aula, escrever a frase com aspas duplas e o número inteiro.
+console.log('Meu nome é "Jefferson". Estou aprendendo JavaScript às', 10, 'da manhã.')
