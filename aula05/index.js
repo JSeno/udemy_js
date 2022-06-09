2866f97a3efd473a15f9c60d3aa747952a5488c6
Jefferson Seno <jefferson.seno@datacake.com.br>
2022-06-08 22:30:54 -0300

    aula_05 udemy js - brincando com alert

---
+++ aula05/index.js
@@ -0,0 +1,8 @@
+alert('olá mundo');
+alert('meu nome é Jefferson');
+alert('Qual é seu nome?');
+var nome = prompt('Qual é seu nome?');
+alert('Olá ' + nome + '!');
+alert('Seja bem vindo!');
+alert('O que você gosta de fazer? ' + nome);
+alert('Ah! que saber tchau!')
