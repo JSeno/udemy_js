2e6db5bd1177b9f300a86cb3c21d2e6fe030ce85
Jefferson Seno <jeffersonseno@datacake.com.br>
2022-07-16 09:15:15 -0300

    Inicio aula 14

---
+++ aula14/index.js
@@ -0,0 +1,7 @@
+let num1 = 1; // number
+let num2 = 2.5; // number
+
+// console.log(num1.toString() + num2); // A toString() converte o numero para string mas não altera o valor do numero
+// num1 = num1.toString();
+// console.log(typeof num1); // number
+// console.log(num1.toString(2)) // Agora a tostring está convertendo o numero para binario
